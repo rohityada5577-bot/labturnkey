86df21fa472cadb19f9b4472163227c9211f474a
himansh-hu <himanshujdubey9@gmail.com>
2026-08-21 14:56:39 +0530

    TEST CHNAGES

--- test-db.php
+++ test-db.php
@@ -1,5 +1,26 @@
 <?php
 
+echo "<h2>Database Test</h2>";
+
+echo "<pre>";
+
+echo "DB_HOST: ";
+var_dump(getenv('DB_HOST'));
+
+echo "\nDB_PORT: ";
+var_dump(getenv('DB_PORT'));
+
+echo "\nDB_DATABASE: ";
+var_dump(getenv('DB_DATABASE'));
+
+echo "\nDB_USERNAME: ";
+var_dump(getenv('DB_USERNAME'));
+
+echo "\nDB_PASSWORD: ";
+echo getenv('DB_PASSWORD') ? "SET" : "NOT SET";
+
+echo "</pre>";
+
 require_once __DIR__ . '/config/database.php';
 
-echo "Database connected successfully!";
+echo "<h2 style='color:green'>DATABASE CONNECTED SUCCESSFULLY!</h2>";
